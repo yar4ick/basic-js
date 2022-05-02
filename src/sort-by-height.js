8637c262125e7da2ff1f950fe9fae33f740b63f1
yar4ick <y.hapalov@gmail.com>
2022-05-02 23:27:56 +0300

    feat: upd tasks

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
+  let filteredArr = [];
+
+  arr.forEach((el) => {
+    if (el != -1) filteredArr.push(el);
+  });
+
+  filteredArr = filteredArr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      filteredArr.splice(i, 0, -1);
+    }
+  }
+
+  return filteredArr;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
--- tst.js
+++ tst.js
@@ -1,46 +1,24 @@
-// function minesweeper(matrix) {
-//   //   throw new NotImplementedError("Not implemented");
-//   // remove line with error and write your code here
-
-//   for (let i = 0; i < matrix.length; i++) {
-//     for (let j = 0; j < matrix[0].length; j++) {
-//       if (matrix[i][j] === true) matrix[i][j] = 1;
-//       if (matrix[i + 1][j + 1] === true)
-//         isNaN(matrix[i][j]) ? matrix[i][j]++ : (matrix[i][j] = 1);
-//       if (matrix[i][j + 1] === true)
-//         isNaN(matrix[i][j]) ? matrix[i][j]++ : (matrix[i][j] = 1);
-//       if (matrix[i + 1][j] === true)
-//         isNaN(matrix[i][j]) ? matrix[i][j]++ : (matrix[i][j] = 1);
-//     }
-//   }
-
-//   return matrix;
-// }
-
-// function getSumOfDigits(n) {
-//   let sum = 0;
-
-//   n.toString()
-//     .split("")
-//     .forEach((element) => {
-//       sum += parseInt(element, 10);
-//     });
-
-//   if (sum >= 10) return getSumOfDigits(sum);
-
-//   return sum;
-// }
-
-// console.log(getSumOfDigits(91));
-
-function sortByHeight(arr) {
-  // throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  //   throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
 
-  return arr.sort((a, b) => {
-    if (a != -1) return a - b;
-    else return a;
-  });
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === true) matrix[i][j] = 1;
+      if (matrix[i + 1][j + 1] === true)
+        isNaN(matrix[i][j]) ? matrix[i][j]++ : (matrix[i][j] = 1);
+      if (matrix[i][j + 1] === true)
+        isNaN(matrix[i][j]) ? matrix[i][j]++ : (matrix[i][j] = 1);
+      if (matrix[i + 1][j] === true)
+        isNaN(matrix[i][j]) ? matrix[i][j]++ : (matrix[i][j] = 1);
+    }
+  }
 }
 
-console.log(sortByHeight([23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3]));
+console.log(
+  minesweeper([
+    [true, false, false],
+    [false, true, false],
+    [false, false, false],
+  ])
+);
